b0e92d24e4ebe914ff61dcd6eea56cd212ac3355
dmays-ellevest <mays.d87@gmail.com>
2022-09-30 20:28:06 -0400

    add objects lesson to hw

---
+++ objects.js
@@ -0,0 +1,15 @@
+let newObject = {
+    name: "Daneen",
+    age: 34, 
+    favColor: "Sage",
+    likesPizza: true,
+} //Oject
+
+//let favoriteColor: newOject.favoriteColor; //variable 
+let favoriteColor = newObject["favColor"];
+console.log(newObject.favColor);
+
+newObject.name = "Daneen Mays";
+
+
+
